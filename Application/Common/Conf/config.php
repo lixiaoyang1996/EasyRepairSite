c212bb39d94badc057ab6edff963af76d9588191
0njzy0 <1120897196@qq.com>
2017-12-25 15:57:53 +0800

    更新配置文件 添加路径变量

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,7 +2,7 @@
 
 return array(
     /* 加载额外配置 */
-    'LOAD_EXT_CONFIG' => 'dbConfig',
+    'LOAD_EXT_CONFIG' => 'dbConfig',// 数据库配置
 
     /* Auth权限认证配置 */
     'AUTH_ON' => true,                      // 认证开关
@@ -14,6 +14,9 @@ return array(
 
     /* 定义常用路径 */
     'TMPL_PARSE_STRING' => array(
-        '__PICTURE__' => __ROOT__ . '/Upload/Pictures',
+        '__PICTURE__' => __ROOT__ . '/Upload/Pictures',// 图片上传路径
+        '__COMPONENTS__' => __ROOT__ . '/Public/components',// 第三方组件路径
+        '__PLUGINS__' => __ROOT__ . '/Public/plugins',// 第三方插件路径
+        '__ADMINLTE__' => __ROOT__ . '/Tpl/Public/adminlte',// adminlte文件路径
     ),
 );

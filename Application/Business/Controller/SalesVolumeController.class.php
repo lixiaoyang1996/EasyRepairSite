01925d546e77fb11b80c3dec2242aec9f983d340
lixiaoyang1996 <15755332575@163.com>
2017-12-27 21:05:59 +0800

    Business控制器中增加了订单管理、店铺管理以及销售数据分布模块以及视图。

---
+++ Application/Business/Controller/SalesVolumeController.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/27
+ * Time: 11:27
+ */
+
+namespace Business\Controller;
+
+use Common\Controller;
+
+class SalesVolumeController extends Controller\BusinessBaseController
+{
+    public function index()
+    {
+        $this->display();
+    }
+}
---
+++ Application/Business/Controller/StoreManageController.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/27
+ * Time: 10:58
+ */
+
+namespace Business\Controller;
+
+use Common\Controller;
+
+class StoreManageController extends Controller\BusinessBaseController
+{
+    public function index()
+    {
+        $this->display();
+    }
+}

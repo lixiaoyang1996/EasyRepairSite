ef35c7c17118f9d44dbc83818d52d57858149ab1
0njzy0 <1120897196@qq.com>
2017-12-29 09:35:19 +0800

    在登录界面增加权限验证

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -23,6 +23,12 @@ class LoginController extends Controller
         $result=$model->where(array('userName'=>$userName,'password'=>$password))->find();
         if($result){
             $_SESSION['userId']=$result['id'];
+            // 判断用户是否有权限登录管理员后台
+            $auth = new \Think\Auth();
+            $result = $auth->check('Admin/*', $_SESSION['userId']);
+            if (!$result) {
+                response('0','登陆失败！');
+            }
             response('1','登陆成功！',array('id'=>$result['id'],'url'=>U('Admin/Index/index')));
         }else{
             response('0','登陆失败！');

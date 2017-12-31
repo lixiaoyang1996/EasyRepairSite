22bdd5abe32e80c03a03f4b86c3940f0a7b9c7b3
0njzy0 <1120897196@qq.com>
2017-12-31 18:26:43 +0800

    修改ajax请求响应函数

--- Application/Admin/Common/function.php
+++ Application/Admin/Common/function.php
@@ -5,13 +5,21 @@
  * Time: 上午8:18
  */
 
-
-function response($code = '', $message = '', $data = array())
+/**
+ * ajax请求响应函数
+ *
+ * @param string $code 响应编号，用户layer弹出层样式变化 1：成功 2：失败
+ * @param string $message 响应信息
+ * @param array $data 返回的响应数据
+ * @param string $url 跳转url
+ */
+function response($code = '', $message = '', $data = array(), $url = '')
 {
     $res = array(
         'code' => $code,
         'message' => $message,
-        'data' => $data
+        'data' => $data,
+        'url' => $url
     );
     exit(json_encode($res));
 }

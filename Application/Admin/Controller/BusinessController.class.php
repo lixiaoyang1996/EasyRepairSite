9a3c25f72c9ab66f42f6f76339db2be531584b84
0njzy0 <1120897196@qq.com>
2018-01-03 09:14:50 +0800

    用户管理功能完成

---
+++ Application/Admin/Controller/BusinessController.class.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * User: njzy
+ * Date: 2017/12/27
+ * Time: 下午8:33
+ */
+
+namespace Admin\Controller;
+
+use Common\Controller\AdminBaseController;
+use Think\Model;
+
+class BusinessController extends AdminBaseController
+{
+    public function index()
+    {
+        $model = new Model();
+        $sql = "SELECT a.* from ers_users as a,ers_auth_group_access as b WHERE a.id=b.uid and b.group_id=2";
+        $users = $model->query($sql);
+        for ($i = 0; $i < count($users); $i++) {
+            $model = M('shop');
+            $shopName=$model->where(array('id'=>$users[$i]['sid']))->getField('name');
+            $users[$i]['shopName'] = $shopName;
+        };
+        $this->assign('users', $users);
+        $this->display();
+    }
+
+    public function add()
+    {
+        $this->display();
+    }
+
+    public function edit()
+    {
+        $id = I('id');
+        $model = M('users');
+        $user = $model->where(array('id' => $id))->find();
+        $this->assign('user', $user);
+        $this->display();
+    }
+
+    public function save()
+    {
+        $model = M('users');
+        $data = $model->create();
+        if ($data['id']) {
+            $res = $model->save($data);
+        } else {
+            $data['password'] = md5($data['password']);
+            $data['register_time'] = time();
+            $data['login_time'] = $data['register_time'];
+            $res = $model->add($data);
+
+            $model = M('auth_group_access');
+            $model->add(array('uid' => $res, 'group_id' => 2));
+        }
+        if ($res >= 0) {
+            response(1, '保存成功！', null, U('Admin/Business/index'));
+        } else {
+            response(2, '保存失败！');
+        }
+    }
+
+    public function del()
+    {
+        $id = I('id');
+        $model = M('users');
+        $res = $model->where(array('id' => $id))->delete();
+        $model = M('auth_group_access');
+        $model->where(array('uid' => $id))->delete();
+        if ($res) {
+            response(1, '删除成功！', null, U('Admin/Business/index'));
+        } else {
+            response(2, '删除失败！');
+        }
+    }
+}
